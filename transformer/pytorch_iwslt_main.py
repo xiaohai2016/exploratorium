ffca63526c605c64325094a71027a0df134a3f86
Xiaohai Zhang <xiaozhang@linkedin.com>
2018-12-27 02:50:53 -0800

    add codes for 4 GPU

--- transformer/pytorch_iwslt_main.py
+++ transformer/pytorch_iwslt_main.py
@@ -13,5 +13,10 @@ def train_on_one_gpu():
     trainer = IWSLTTrainer(use_gpu=True, devices=[0])
     trainer.run_training(10, log_interval=1)
 
+def train_on_four_gpu():
+    """Run training for IWSLT on CPU for debugging purpose"""
+    trainer = IWSLTTrainer(use_gpu=True, devices=[0, 1, 2, 3])
+    trainer.run_training(10, log_interval=1)
+
 if __name__ == "__main__":
-    train_on_one_gpu()
+    train_on_cpu()
