5707038df74535656a871fafde11c482fa7ed225
Xiaohai Zhang <xiaozhang@linkedin.com>
2018-12-28 14:26:24 -0800

    put batch_size as a parameter

--- transformer/pytorch_iwslt_main.py
+++ transformer/pytorch_iwslt_main.py
@@ -3,14 +3,14 @@ The Pytorch IWSLT operational main file
 """
 from pytorch_iwslt_training import IWSLTTrainer
 
-def train_on_cpu():
+def train_on_cpu(batch_size=6000):
     """Run training for IWSLT on CPU for debugging purpose"""
-    trainer = IWSLTTrainer(batch_size=4000)
+    trainer = IWSLTTrainer(batch_size=batch_size)
     trainer.run_training(10, log_interval=1)
 
-def train_on_one_gpu():
+def train_on_one_gpu(batch_size=6000):
     """Run training for IWSLT on CPU for debugging purpose"""
-    trainer = IWSLTTrainer(use_gpu=True, devices=[0], batch_size=4000)
+    trainer = IWSLTTrainer(use_gpu=True, devices=[0], batch_size=batch_size)
     trainer.run_training(10, log_interval=10)
 
 def train_on_two_gpu():
